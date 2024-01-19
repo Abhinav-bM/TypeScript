e1c15c2cf1641d95f37c8c5f4e617c0ee1cf8cee
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-19 11:34:21 +0530

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+// infer types (implicit types])
+var college = 'm-DIT';
+// // defining types (explicit types)
+var myName = 'abhinav';
+var subscriber = 2000;
+var isSubscribed = true;
+var skills = ['js', 'css', 'ts'];
+var numberArr = [1, 2, 3, 4, 5];
+var emptyArray = [];
+var usersDetails = {
+    name: "abhinav",
+    age: 22
+};
+var workerDetails = {
+    name: 'rahul',
+    age: 22,
+    salary: 20000,
+    getName: function () {
+        console.log(this.name);
+    }
+};
+
+
--- index.ts
+++ index.ts
@@ -1,10 +1,59 @@
 // infer types (implicit types])
-let userName  = 'abhinav'
-userName = ";lejhdkjgdj"
+let college:string  = 'm-DIT'
 
 
-// defining types (explicit types)
+// // defining types (explicit types)
 let myName:string = 'abhinav'
 let subscriber:number = 2000;
 let isSubscribed:boolean = true
 let skills:string[] = ['js','css','ts']
+let numberArr:number[] = [1,2,3,4,5]
+let emptyArray:[] = []
+
+
+let usersDetails:{ name : string; age : number } = {
+    name:"abhinav",
+    age:22
+}
+
+
+// interface 
+// interface Details {
+//     name : string;
+//     age : number;
+//     salary : number;
+//     getName: ()=>void;
+// }
+ 
+// let workerDetails : Details = {
+//     name : 'rahul',
+//     age : 22,
+//     salary : 20000,
+//     getName(){
+//         console.log(this.name)
+//     }
+// }  
+
+// type
+type Details {
+    name : string;
+    age : number;
+    salary : number;
+    getName: ()=>void;
+}
+ 
+let workerDetails : Details = {
+    name : 'rahul',
+    age : 22,
+    salary : 20000,
+    getName(){
+        console.log(this.name)
+    }
+}
+
+function getUserName (workerDetails:Details){     // workerDetails:Details , it will give all the suggestion that inside the object if we done like this
+    return workerDetails.age     
+}
+
+
+
