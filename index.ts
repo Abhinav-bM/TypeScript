5b90815166ac85158ed1402496dcbaecfb26db11
Abhinav-bM <abhinavbmanoj@gmail.com>
2024-01-19 10:14:15 +0530

    basics

---
+++ index.ts
@@ -0,0 +1,10 @@
+// infer types (implicit types])
+let userName  = 'abhinav'
+userName = ";lejhdkjgdj"
+
+
+// defining types (explicit types)
+let myName:string = 'abhinav'
+let subscriber:number = 2000;
+let isSubscribed:boolean = true
+let skills:string[] = ['js','css','ts']
